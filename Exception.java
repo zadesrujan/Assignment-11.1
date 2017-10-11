09e0539da41ccaee617fc1335ae09cbd6612778c
zadesrujan <31057499+zadesrujan@users.noreply.github.com>
2017-10-11 11:57:49 +0530

    Add files via upload

---
+++ Exception.java
@@ -0,0 +1,30 @@
+package integerarray;
+//here we cretaed package as integerarray which that organizes a set of related classes and interfaces.
+public class Exception {
+
+	public static void main(String[] args) {
+			//public : it can be called from anywhere.
+			//static : doesn't belong to a specific object.
+			//void : returns to no value.
+			//main : is special because it will start the program.
+		// TODO Auto-generated method stub
+
+		int s[]=new int[5];
+		//created an array with size of 5
+		s[0]=25;
+		s[1]=12;
+		s[2]=56;
+		s[3]=84;
+		s[4]=96;
+		System.out.println(s[5]=70);
+		//system : is a class in java language pacakge.
+		//out : static member of the system class.
+		//println : to print what is output.
+		//here we are trying to print element 6 but we can't we get 
+		
+	}
+
+
+	}
+
+
